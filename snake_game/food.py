82c2533a70c7bfd7cfd4035d900d12898e5427dc
Zoe Oladokun <145172902+zozoking101@users.noreply.github.com>
2024-03-01 23:44:54 +0100

    food scoreboard commit

---
+++ snake_game/food.py
@@ -0,0 +1,18 @@
+from turtle import Turtle
+from random import randint
+
+
+class Food(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.penup()
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
+        self.color("red")
+        self.speed("fastest")
+        self.refresh()
+
+    def refresh(self):
+        rand_x = randint(-280, 281)
+        rand_y = randint(-280, 281)
+        self.goto(rand_x, rand_y)
--- snake_game/main.py
+++ snake_game/main.py
@@ -1,6 +1,8 @@
 from turtle import Screen
-import time
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+import time
 
 
 screen = Screen()
@@ -8,8 +10,12 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)
-screen.listen()
+
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
@@ -22,5 +28,9 @@ while game_is_on:
     time.sleep(0.1)
     snake.move()
 
-
+    #Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        scoreboard.increase_score()
+        
 screen.exitonclick()
